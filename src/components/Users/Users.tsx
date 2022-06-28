37ca505658813b78d5af07cc3b42f46b9a86511c
akr7777 <dimitrius77@protonmail.com>
2022-06-28 23:31:48 +0400

    lesson 54 done

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -15,11 +15,14 @@ type UsersPropsType1 = {
 
 class Users extends React.Component<any, any> {
 
-    constructor(props: any) {
+    /*constructor(props: any) {
         super(props);
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            this.props.setUsers(response.data.items);
+        });
+    }*/
 
-        //alert('New users');
-
+    componentDidMount() {
         axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
             this.props.setUsers(response.data.items);
         });
